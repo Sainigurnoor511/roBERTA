879eb8e6091aca7f1b64e14ff13ffce368e87326
Gurnoor Singh Saini <92860451+Sainigurnoor511@users.noreply.github.com>
2024-08-12 06:11:06 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+from transformers import pipeline
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Sentiment Analysis using rBERTa model")
+st.write("An app to analyse your reviews")
+
+
+user_input = st.text_input("Please enter your review >>: ")
+
+specific_model = pipeline(model="cardiffnlp/twitter-roberta-base-sentiment-latest")
+
+
+if (specific_model(user_input)[0]['label']) == 0:
+    st.write(" ")
+elif (specific_model(user_input)[0]['label']) == 1 :
+    st.write("# 😊 Positive")
+elif (specific_model(user_input)[0]['label']) == "NEU" :
+    st.write("# 😐 Neutral")
+elif (specific_model(user_input)[0]['label']) == "NEG" :
+    st.write("# 😠 Negative")
+else :
+    st.write("# ☠️ Error ☠️")
